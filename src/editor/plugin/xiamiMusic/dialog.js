54543a2c057487db7556b1dcb5d576c1183de39a
yiminghe <yiminghe@gmail.com>
2012-04-18 23:01:16 +0800

    merge with KISSY

--- src/editor/plugin/xiamiMusic/dialog.js
+++
@@ -1,8 +0,0 @@
-KISSY.Editor.add("xiamiMusic/dialog", function(editor) {
-    var KE = KISSY.Editor;
-    KE.use("xiamiMusic/dialog/support", function() {
-        editor.addDialog("xiamiMusic/dialog", new KE.XiamiMusic.Dialog(editor));
-    });
-},{
-    attach:false
-});
